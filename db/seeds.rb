6f7710861f5ddbdc653b2731ea133a9ab62d495d
BilalAbbasAwan <bilalabbas437@gmail.com>
2018-12-15 01:08:27 +0500

    Admin By Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+		 store_admin = User.new({ name: "GigaLabs", email: 'gigalabs@gmail.com',
+		               password: '72a22ctevta'})
+
+		    if store_admin.save
+		      store_admin.add_role :store_admin
+		      shop = Shop.create(store_admin_id: store_admin.id, name: 'Anarkalli Febrics', email: 'anarkalli@gmail.com', phone: 923015853980, address: 'Chowk Ehl-e-Hadees Jhelum')
+
+		    elsif admin.errors.any?
+		      admin.errors.full_messages.each do |msg|
+		        puts msg
+		      end
+		    else
+		      puts "****NOT VALID****"
+		end
